e017e242d87c52e7ab4594dbd77c63eeffcfbc73
Liam Cohron <therealkiro0613@gmail.com>
2020-02-24 13:17:36 -0500

    I made visibility work

--- Assets/Scripts/Player/PlrVisibility.cs
+++ Assets/Scripts/Player/PlrVisibility.cs
@@ -25,6 +25,7 @@ namespace Player {
 
         // Update is called once per frame
         void Update() {
+            FindLights();
             visMeter.text = "Visibility: " + FormatVis() + "%";
 
             if(Input.GetKeyDown(KeyCode.T)) {
@@ -38,13 +39,38 @@ namespace Player {
                 prettyVis = 1;
             }
 
-            prettyVis = Mathf.Round(vis * 100f);
+            prettyVis = Mathf.Round(prettyVis * 100f);
 
             return prettyVis;
         }
 
         void FindLights() {
-            GameObject[] lights = GameObject.FindGameObjectsWithTag("Light");
+            GameObject[] allLights = GameObject.FindGameObjectsWithTag("Light");
+            List<GameObject> lights = new List<GameObject>();
+            foreach(GameObject light in allLights) {
+                if(this.sees(light)) {
+                    lights.Add(light);
+                }
+            }
+
+            vis = 0f;
+
+            if(lights.Count == 0) {
+                return;
+            }
+
+            foreach(GameObject lightObj in lights) {
+                float distToLight = (lightObj.transform.position - transform.position).magnitude;
+                Light light = lightObj.GetComponent<Light>();
+
+                if(distToLight < light.range) {
+                    if(distToLight == 0) {
+                        vis += 100;
+                    } else {
+                        vis += (1 - (distToLight / light.range)) * 100;
+                    }
+                }
+            }
         }
 
         void SeeTest() {
